621290b93c72491080e3778d0750961b2a43a5ac
hejincheng <1044492138@qq.com>
2017-08-09 00:10:18 +0800

    activity v3

--- application/views/dashboard/unbind/find_user.php
+++ application/views/dashboard/unbind/find_user.php
@@ -1,6 +1,38 @@
-<?php
-foreach($user as $row)
-{
-	print_r($row);
-	echo "<br>";
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<style type="text/css">
+table{
+	width: 90%;
+	margin:0 auto;
+	padding-top: 30px;
+	text-align: center;
 }
+tr{
+	height: 40px;
+	border-bottom: 2px solid white;
+}
+tr:hover{
+	background-color: rgba(224, 212, 212, 0.17);
+}
+td{
+	border-right: 1px solid white;
+}
+</style>
+</head>
+<body>
+<table>
+	<tr style="background-color: rgba(199, 189, 189, 0.16)">
+		<td style="width: 12%;">ID</td>
+		<td style="max-width:18%;">学号</td>
+		<td style="width: 20%;">姓名</td>
+		<td style="width: 20%;">卡号</td>
+		<td colspan="2" style="width: 20%;">操作</td>
+	</tr>
+	<tbody> 
+		 
+	</tbody>
+</table>
+</body>
+</html>
